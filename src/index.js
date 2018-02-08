c86cd0277162fedbd95a7363fa38d8769252e1cf
Marta Aguilar <martaguilar.soto@gmail.com>
2018-02-08 09:18:26 +0100

    create project firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyAbzw_iuCC7wya88lxix0BoZ1kKR9vU3Ic",
+    authDomain: "timetracking-coddde.firebaseapp.com",
+    databaseURL: "https://timetracking-coddde.firebaseio.com",
+    projectId: "timetracking-coddde",
+    storageBucket: "timetracking-coddde.appspot.com",
+    messagingSenderId: "377778818961"
+
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
