ad24463bbc5cf3947e12de5aa6a0ac6512969942
Icia <icia83@gmail.com>
2018-02-09 00:38:37 +0100

    add tasks and counter list

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,26 +4,19 @@ class Counter extends React.Component {
 	constructor (props) {
 		super(props)
 
+		this.startTimer = this.startTimer.bind(this);
+		this.pauseTimer = this.pauseTimer.bind(this);
+		this.stopTimer = this.stopTimer.bind(this);
+		this.paintTasks = this.paintTasks.bind(this);
+		this.formatTime = this.formatTime.bind(this);
+
 		this.state = {
 			count: 0,
-			secondsCount: 0,
-			minsCount: 0,
-			hoursCount: 0,
 			customNumber: 0,
-			lastInput: 0
+			lastInput: 0,
+			tasks: [],
+			stopClick: false
 		}
-	}
-
-	handleChange (e) {
-		const value = e.target.value;
-		this.setState({
-			customNumber: value,
-			lastInput: value
-		});
-	}
-
-	componentWillUnmount () {
-		clearInterval(this.timer)
 	}
 
 	tick () {
@@ -41,16 +34,21 @@ class Counter extends React.Component {
 			}
 	}
 
-	display () {
-		let hours = Math.floor(this.state.count / 360);
-		let minutes = Math.floor((this.state.count - (hours * 60)) / 60);
-		let seconds = Math.floor((this.state.count - (minutes * 60)));
+	formatTime (number) {
+		let hours = Math.floor(number / 3600);
+		let minutes = Math.floor((number - (hours * 60)) / 60);
+		let seconds = Math.floor((number - (minutes * 60)));
 
+		//pasamos los valores a un string para poder pintarlos después en el return
 		let hoursStr = (hours < 10) ? ('0' + hours) : '' + hours;
 		let minutesStr = (minutes < 10) ? ('0' + minutes) : '' + minutes;
 		let secondsStr = (seconds < 10) ? ('0' + seconds) : '' + seconds;
 
-		return (hoursStr + ':' + minutesStr + ':' + secondsStr);
+		return `${hoursStr}:${minutesStr}:${secondsStr}`;
+	}
+
+	display () {
+		return this.formatTime(this.state.count);//usamos la función formatTime para el contador
 	}
 
 	startTimer () {
@@ -59,25 +57,48 @@ class Counter extends React.Component {
 		this.setState({ disabled: true })
 	}
 
+	pauseTimer () {
+		clearInterval(this.timer)
+	}
+
 	stopTimer () {
 		clearInterval(this.timer)
+		//recojo la referencia al array de tareas y al contador actual
+		const { tasks, count } = this.state;
+		tasks.push({taskName: 'taskName', counter: count}); //se añade una tarea nueva al array de tareas que luego tendremos que cambiar por el input que estemos usando
+		this.setState({
+			count: 0,
+			stopClick: true,
+			tasks: tasks //sobreescribe el array de tasks
+		});
 	}
 
-	resetTimer () {
-		clearInterval(this.timer)
-		this.setState({count: 0, customNumber: this.state.lastInput})
-		this.setState({ disabled: false })
-	}
+	paintTasks() {
+    let tasksToShow = this.state.tasks; //esto es como el ejemplo de los perros de Isra
+		return (
+			<ul>
+				{tasksToShow.map(
+					task => <li>
+						{task.taskName}, {this.formatTime(task.counter)}
+					</li>
+				)}
+			</ul>);
+  }
+
+
+
 
 	render () {
+
 		return (
 			<div className='timer'>
-				<h1 >{this.display()}</h1>
+				<counter >{this.display()}</counter>
 				<div  className="buttons">
-					<button  type="button" name="start_btn" id="start_btn" onClick={this.startTimer.bind(this)}>Start</button>
-					<button  type="button" name="stop_btn" id="stop_btn" onClick={this.stopTimer.bind(this)}>Pause</button>
-					<button  type="button" name="reset_btn" id="reset_btn" onClick={this.resetTimer.bind(this)}>Stop</button>
+					<button  type="button" name="start_btn" id="start_btn" onClick={this.startTimer}>Start</button>
+					<button  type="button" name="stop_btn" id="stop_btn" onClick={this.pauseTimer}>Pause</button>
+					<button  type="button" name="reset_btn" id="reset_btn" onClick={this.stopTimer}>Stop</button>
 				</div>
+				{this.paintTasks()}
 			</div>
 		)
 	}
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -11,7 +11,7 @@ class Timer extends React.Component{
 		this.state = {
 			 hours: '',
 			 minutes: '',
-			 seconds:''
+			 seconds: ''
 		 }
  }
 
