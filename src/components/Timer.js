b2ed23e24f67eab60a610bce2dfbf1cf315f8595
mluena <maluenarod@gmail.com>
2018-02-09 11:15:55 +0100

    creating component database

---
+++ src/components/Timer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Clock extends React.Component{
+	constructor (props) {
+		super(props)
+
+		this.updateClock=this.updateClock.bind(this);
+
+		setInterval (this.updateClock,1000);
+
+		this.state = {
+			 hours: new Date().getHours(),
+			 minutes: new Date().getMinutes(),
+			 seconds: new Date().getSeconds()
+		 }
+ }
+
+ updateClock () {
+	 this.setState ({
+ 		 hours: new Date().getHours(),
+ 		 minutes: new Date().getMinutes(),
+ 		 seconds: new Date().getSeconds()
+ 	 })
+ }
+
+	render(){
+		return(
+
+			<div>
+			{this.state.hours}:
+			{this.state.minutes}:
+			{this.state.seconds}
+			</div>
+		);
+	}
+}
+
+export default Timer;
---
+++ src/pages/Login.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import firebase from 'firebase';
+
+class Login extends React.Component {
+	constructor (props) {
+		super (props)
+		this.state = {
+			email: '',
+			password: ''
+		}
+		this.handleAuthGoogle = this.handleAuthGoogle.bind(this);
+		this.handleAuthEmail = this.handleAuthEmail.bind(this);
+	}
+
+	handleAuthGoogle () {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithPopup(provider)
+			.then(result => console.log(result, 'ha iniciado sesión'))
+			.catch(error => console.log(`Error ${error.code}:${error.message}`));
+	}
+
+	handleAuthEmail () {
+		const email = this.state.email;
+		const password = this.state.password;
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(result => console.log(result, 'ha iniciado sesión'))
+		  .catch(error => console.log(`Error ${error.code}:${error.message}`));
+			console.log(email, password)
+	}
+
+	handleInputChange(input, value) {
+		this.setState({
+			[input]: value
+		});
+	}
+
+  render() {
+    return (
+			<div className="form">
+				<header className="form-header">
+					<h1 className="form-title">Time tracker</h1>
+				</header>
+				<form className="login__form">
+					<div className="login__inputs">
+						<input name="onLoginSuccess" value={this.state.email} className="login__input" type="text" placeholder="E-mail" ref="email" onChange={e => this.handleInputChange('email', e.target.value)}/>
+						<input value={this.state.password} className="login__input" type="password"  placeholder="Password" ref="password" onChange={e => this.handleInputChange('password', e.target.value)}/>
+					</div>
+					<div className="login__buttons">
+						<button className="login__button" type="button" onClick={this.handleAuthEmail}>Log-in</button>
+						<button className="login__button" type="button" onClick={this.handleAuthGoogle}>Log in con Google
+						</button>
+					</div>
+				</form>
+			</div>
+    );
+  }
+}
+
+export default Login;
