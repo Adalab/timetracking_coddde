eb68bc98fb0ee511342c1108cf9d67a0c8e838c8
mluena <maluenarod@gmail.com>
2018-02-09 18:27:53 +0100

    first user completed

--- src/components/Databasetest.js
+++ src/components/Databasetest.js
@@ -6,21 +6,45 @@ class Database extends React.Component {
 	constructor (props) {
 		super (props)
 		this.state = {
-			name: ''
+			name: '',
+			surname: '',
+			email: '',
+			password: ''
 		}
 	}
 
 	componentWillMount () {
 		const nameRef = firebase.database().ref().child('usuario').child('name')
+		const surnameRef = firebase.database().ref().child('usuario').child('surname')
+		const emailRef = firebase.database().ref().child('usuario').child('email')
+		const passwordRef = firebase.database().ref().child('usuario').child('password')
 
 		nameRef.on('value',(snapshot) => {
 			this.setState({
 				name: snapshot.val()
 			})
 		})
+		surnameRef.on('value',(snapshot) => {
+			this.setState({
+				surname: snapshot.val()
+			})
+		})
+		emailRef.on('value',(snapshot) => {
+			this.setState({
+				email: snapshot.val()
+			})
+		})
+		passwordRef.on('value',(snapshot) => {
+			this.setState({
+				password: snapshot.val()
+			})
+		})
 	}
 	render() {
-		return <h1>Hola {this.state.name}</h1>
+		return (
+			<div>
+				<h1>Hola {this.state.name}{this.state.surname} con email {this.state.email} y contraseña {this.state.password}</h1>
+			</div>)
 	}
 }
 
