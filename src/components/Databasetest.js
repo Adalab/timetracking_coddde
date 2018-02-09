9753f7bb475cc90b53f5a8f1052e53a5571c7a53
mluena <maluenarod@gmail.com>
2018-02-09 18:05:43 +0100

    first user name database

--- src/components/Databasetest.js
+++ src/components/Databasetest.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import firebase from 'firebase';
 
 
 class Database extends React.Component {
+	constructor (props) {
+		super (props)
+		this.state = {
+			name: ''
+		}
+	}
+
+	componentWillMount () {
+		const nameRef = firebase.database().ref().child('usuario').child('name')
+
+		nameRef.on('value',(snapshot) => {
+			this.setState({
+				name: snapshot.val()
+			})
+		})
+	}
 	render() {
-		return();
+		return <h1>Hola {this.state.name}</h1>
 	}
 }
 
