522c43334eb1cd5c8feff15492c02919b5e77fc9
Marta Aguilar <martaguilar.soto@gmail.com>
2018-02-12 13:27:33 +0100

    delete conunter and hour duplicate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Loading from './components/Loading';
-import Timer from './components/Timer';
-import Counter from './components/Counter';
 import Graphic from './components/Graphic';
 
 class App extends Component {
@@ -9,8 +7,6 @@ class App extends Component {
     return (
       <div className="App">
       	<Loading />
-				<Timer />
-				<Counter />
 				<input className="calendar" type="date" value="today"></input>
 				<Graphic />
       </div>
